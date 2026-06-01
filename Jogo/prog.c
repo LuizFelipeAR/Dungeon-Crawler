284d25d9bbcd844d0cda369f2039388e79d8174c
Pedro Arthur Souza <pedroarthurfsouza@gmail.com>
2026-06-01 19:10:24 +0000

    cadrado

--- Jogo/prog.c
+++ Jogo/prog.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 int main(){
 
-printf("fanhsidahdya");
-    return 0; 
+int i;
+int j;
+
+printf("\n\nTESTE primeiro\n");
+
+for(j=0;j<5;j++){  
+for(i=0;i<8;i++){
+    printf("#");
+} 
+printf("\n");
+}
+    
+
+
+
+return 0; 
 }
