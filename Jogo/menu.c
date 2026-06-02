d19cfefaaa548377977681148c023b6211d9ebfd
Pedro Arthur Souza <pedroarthurfsouza@gmail.com>
2026-06-02 13:51:25 +0000

    criei o menu

--- Jogo/menu.c
+++ Jogo/menu.c
@@ -2,6 +2,41 @@
 
 int main(){
 
+int opcao;
+
+
+while(1){
+
+    
+printf("Bem Vindo ao DUNGEON CRAWLER!\n");
+
+printf("1. Jogar - Inicia o jogo a partir da vila\n");
+printf("2. Tutorial - Exibe a historia do jogo, significado de cada simbolo e controles\n");
+printf("3. Sair");
+
+scanf("%d",&opcao);
+
+  switch(opcao) {
+        case 1:
+            printf("Você escolheu JOGAR.\n");
+            // void(jogo);
+            break;
+        case 2:
+            printf("Você escolheu TUTORIAL.\n");
+            // void(tutorial);
+            break;
+        case 3:
+            printf("Saindo do programa...\n");
+            break;
+        default:
+            printf("Opção inválida! Tente novamente.\n");
+    }
+
+if(opcao == 3){
+    break;
+}
+
+}
 
     return 0;
 }
