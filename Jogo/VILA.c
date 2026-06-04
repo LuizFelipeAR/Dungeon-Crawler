7973db1242755371cc8d403eb805186ed14fd36a
Ricardo Kenji <rcrdknj@gmail.com>
2026-06-04 18:14:49 -0300

    [ADD] Adicionei a base da Vila: renderizacao do mapa, carregamento, movimentacao com WASD e colisao, direcao persistente do jogador, interacao com o NPC pela tecla i e menu de escolha de arma com validacao.

--- Jogo/VILA.c
+++ Jogo/VILA.c
@@ -9,7 +9,7 @@ const char *MAPA_VILA[10] = {
     "*        *",
     "*        *",
     "*        *",
-    "*        *",
+    "*    N   *",
     "*        *",
     "*        *",
     "*        *",
@@ -20,6 +20,7 @@ char terreno[MAX][MAX];
 int altura, largura;
 int jogador_linha = 1, jogador_coluna = 1; // Posicao do Jogador
 char jogador_dir = '>'; // Direcao que o jogador olha
+int arma = 0; // 0 = nenhuma ainda; 1 = espada, 2 = arco, 3 = cajado
 
 void carregar_vila(void) {
     for (int i = 0; i < altura; i++)
@@ -44,9 +45,38 @@ void mover(int dx, int dy, char dir) {
     jogador_dir = dir; // Vira mesmo batendo na parede
     int novo_linha = jogador_linha + dy;
     int novo_coluna = jogador_coluna + dx;
-    if (terreno[novo_linha][novo_coluna] != '*') {
+    if (terreno[novo_linha][novo_coluna] != '*' && terreno[novo_linha][novo_coluna] != 'N') {
         jogador_coluna = novo_coluna;
         jogador_linha = novo_linha;
+    }
+}
+
+void interagir(void) {
+    int frente_linha = jogador_linha;
+    int frente_coluna = jogador_coluna;
+
+    if (jogador_dir == '^')      frente_linha--;
+    else if (jogador_dir == 'v') frente_linha++;
+    else if (jogador_dir == '<') frente_coluna--;
+    else if (jogador_dir == '>') frente_coluna++;
+
+    if (terreno[frente_linha][frente_coluna] == 'N') {
+        int escolha = 0;
+
+        while (escolha < 1 || escolha > 3) { // Repete enquanto for invalida
+            printf("\n=== NPC ===\n");
+            printf("Escolha sua arma:\n");
+            printf("1 - Espada\n");
+            printf("2 - Arco e Flecha\n");
+            printf("3 - Cajado\n");
+            printf("Opcao: ");
+            scanf(" %d", &escolha);
+
+            if (escolha < 1 || escolha > 3)
+                printf("Opcao invalida! Tente de novo.\n");
+        }
+
+        arma = escolha; // So chega aqui quando a escolha e valida
     }
 }
 
@@ -64,6 +94,7 @@ int main(void) {
         else if (entrada == 's') mover(0,  1, 'v');
         else if (entrada == 'a') mover(-1, 0, '<');
         else if (entrada == 'd') mover( 1, 0, '>');
+        else if (entrada == 'i') interagir(); //A tecla de interacao
         else break;
     }
     
